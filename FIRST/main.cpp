8ee7badf6697ea2abf7461f4233c167022cc05e8
RyanPerkins <bobefett.qwert12@gmail.com>
2019-01-22 09:18:07 -0700

    created the first c++ project

---
+++ FIRST/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  FIRST
+//
+//  Created by Perkins, Ryan on 1/22/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // insert code here...
+    cout << "1-800-NO-PEN15\n";
+    return 0;
+}
